2b386bfdec40a84026b4cae1982c1ad745bea0e2
samir jadhav <samirzjadhav@gmail.com>
2024-07-13 18:19:19 +0200

    feat: added react icons and fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,30 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        lightYellow: "#f6d876",
+        white2: "#fffbf2",
+        darkGreen: "#05ab54",
+        dark: "#1e1e1e",
+      },
+
+      fontFamily: {
+        sans: ["Poppins", "sans-serif"],
+        league: ["League Gothic", "sans-serif"],
+      },
+
+      container: {
+        center: true,
+        padding: {
+          DEFAULT: "1rem",
+          sm: "2rem",
+          lg: "4rem",
+          xl: "5rem",
+          "2xl": "6rem",
+        },
+      },
+    },
   },
   plugins: [],
 };
