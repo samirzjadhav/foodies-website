c5c94772b9f75685b87f1594da3dadb18a12c3b3
samir jadhav <samirzjadhav@gmail.com>
2024-07-25 17:52:59 +0530

    feat: added popular recipes and animate it

--- src/Components/PopularRecipe/PopularRecipe.js
+++ src/Components/PopularRecipe/PopularRecipe.js
@@ -39,6 +39,27 @@ const PopularRecipe = () => {
           >
             Our Popular Recipe
           </motion.h3>
+          {/* Card Section  */}
+          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6 place-item-center">
+            {PopularRecipeData.map((item) => {
+              return (
+                <div className="group space-y-3 text-center bg-white/50 shadow-xl p-3 rounded-xl">
+                  <img
+                    src={item.img}
+                    className="w-44 h-44 mx-auto img-shadow group-hover:scale-x-110 group-hover:translate-y-[-50px] group-hover:translate-x-10 group-hover:rotate-[50deg] transition-all duration-700"
+                    alt=""
+                  />
+                  <button className="btn-primary group-hover:mb-3 opacity-0 group-hover:opacity-100">
+                    Buy now
+                  </button>
+                  <p className="text-xl font-semibold">{item.name}</p>
+                  <p className="text-xl font-bold text-yellow-500">
+                    {item.price}
+                  </p>
+                </div>
+              );
+            })}
+          </div>
         </div>
       </section>
     </>
